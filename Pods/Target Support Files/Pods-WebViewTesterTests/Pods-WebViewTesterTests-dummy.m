1618ce0393dbb261c9ce7aac3e1d781483fc2811
indrajit-roy-sc <indrajit.roy@smallcase.com>
2024-05-21 11:02:32 +0530

    refactor: rename entire project to `SmartInvesting` from `WebViewTester`

--- Pods/Target Support Files/Pods-WebViewTesterTests/Pods-WebViewTesterTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WebViewTesterTests : NSObject
-@end
-@implementation PodsDummy_Pods_WebViewTesterTests
-@end
--- Pods/Target Support Files/Pods-WebViewTesterUITests/Pods-WebViewTesterUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WebViewTesterUITests : NSObject
-@end
-@implementation PodsDummy_Pods_WebViewTesterUITests
-@end
