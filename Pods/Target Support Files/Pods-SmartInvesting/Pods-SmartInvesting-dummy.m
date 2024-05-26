b8f82e4fbc7c0f502ced20ef6aefc2c697374dbc
indrajit-roy-sc <indrajit.roy@smallcase.com>
2024-05-26 18:23:46 +0530

    Remove all Pods from the Git cache

--- Pods/Target Support Files/Pods-SmartInvesting/Pods-SmartInvesting-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SmartInvesting : NSObject
-@end
-@implementation PodsDummy_Pods_SmartInvesting
-@end
--- Pods/Target Support Files/Pods-SmartInvestingTests/Pods-SmartInvestingTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SmartInvestingTests : NSObject
-@end
-@implementation PodsDummy_Pods_SmartInvestingTests
-@end
